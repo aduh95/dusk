0193790c45abec268e269522611ba282af12bae0
Antoine du Hamel <duhamelantoine1995@gmail.com>
2021-01-11 17:03:19 +0100

    Fix Outlook script

--- outlook.js
+++ outlook.js
@@ -16,7 +16,7 @@
     return e;
   }
   async function u() {
-    const s = document.getElementById("owaSettingsButton");
+    const s = await w("#owaSettingsButton");
     const c = !s.classList.contains("o365sx-activeButton");
     c && s.click();
     const t = await w(".ms-Toggle-background");
@@ -26,17 +26,17 @@
     c && s.click();
   }
   function c(options) {
-    if (options?.theme == null || options.theme === "auto") {
+    if (typeof options.theme === "string")
+      options.theme = { newValue: options.theme };
+    if (options?.theme == null || options.theme.newValue === "auto") {
       m.addEventListener("change", u);
       isDark = returnAuto;
     } else {
       m.removeEventListener("change", u);
-      isDark = options.theme === "dark" ? returnTrue : returnFalse;
+      isDark = options.theme.newValue === "dark" ? returnTrue : returnFalse;
     }
     u();
   }
   chrome.storage.onChanged.addListener(c);
-  addEventListener("load", () => chrome.storage.sync.get("theme", c), {
-    once: true,
-  });
+  chrome.storage.sync.get("theme", c);
 }
